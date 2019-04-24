9c93214a5dab956442c8175255001c9fbc943913
aomine <xomarxpl07@outlook.es>
2019-04-23 21:58:48 -0500

    inicio-nosotros v0.1

--- Aomine/README.md
+++ Aomine/README.md
@@ -1,46 +1,10 @@
-# Template Single Page | NibbleFrame
-Caracteristicas
-* scrollspy
-* responsive
-* sidenav drag out
-* Messages Services
+# PixeLand | aomine
 
-## Librerias
-* FontAwesome
-* Animate on Scroll4
+## Cambios | Añadidos
+### Variables
+* añadiendo variables de colores de marca
+* comentando en general
+* añadido link-primary
 
-## Arquitectura Sass
-Organización de carpetas y archivos Sass.
-
-### Priority
-Elementos prioritarios
-* loading
-
-### Lib
-Herramientas y helpers de Sass
-* base colors
-* variables
-* mixins-media
-* functions
-* placeholders
-* mixins
-
-### Global
-Estilos y clases globales
-* reset
-* generals
-* class
-
-### Layout
-Estilos de los layouts
-* header
-* navigation
-* inicio
-* nosotros
-* servicios
-* pagos
-* portafolio
-* clientes
-* blog
-* contactanos
-* footer
+## Errores
+* font text - placeholder
--- Aomine/assets/js/main.js
+++ Aomine/assets/js/main.js
@@ -1,7 +1,70 @@
-//DOM Loaded
-window.addEventListener('DOMContentLoaded', function() {
-	const loader = new Loader({
-		id: 'loader'
-	});
-	loader.start();
+// Variables DOM
+const $body = document.body,
+$nav = document.querySelector('.nav'),
+$navItems = document.querySelectorAll('.nav-item');
+
+// Breakpoints
+const $desktopBpMd = window.matchMedia('only screen and (max-width: 991px)');
+
+// Instanciando Objetos
+// Loader
+const loader = new Loader({
+	id: 'loader'
 });
+NProgress.start();
+
+// Scrollspy
+const scrollspy = new Scrollspy({
+	header: '.header',
+	sections: 'section',
+	active: 'nav-link-active'
+});
+
+
+// On Load
+window.addEventListener('load', function(){
+	// Loader
+	loader.done();
+	NProgress.done();
+
+	// Scrollspy
+	scrollspy.ini();
+});
+
+// On Scroll
+window.addEventListener('scroll', function(){
+	// Scrollspy
+	scrollspy.spy();
+})
+
+// On Resize
+window.addEventListener('resize', function(){
+	// Scrollspy
+	scrollspy.ini();
+});
+
+//DOM Loaded
+window.addEventListener('DOMContentLoaded', function(){
+	// Media Queries
+	const mql = e => {
+		var instances;
+		if (e.matches) {
+			$nav.style.transform = 'translateX(-105%)';
+			instances = M.Sidenav.init($nav);
+			for(let i of $navItems){
+				i.classList.add('waves');
+			}
+		}else{
+			$body.style.overflow = '';
+			$nav.style.transform = 'translateX(0px)';
+			for(let i of $navItems){
+				i.classList.remove('waves');
+			}
+		}
+	}
+
+	$desktopBpMd.addListener(mql);
+	mql($desktopBpMd);
+});
+
+
