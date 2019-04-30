19bdd4decc6409082165a89be208bf8d890fca41
aomine <xomarxpl07@outlook.es>
2019-04-30 13:53:26 -0500

    inicio-nosotros v0.7

--- Aomine/assets/js/main.js
+++ Aomine/assets/js/main.js
@@ -25,6 +25,11 @@ const scrollspy = new Scrollspy({
 const header = new Header({
 	header: '.header',
 	active: 'header-active',
+	nixe: '.logo-img'
+});
+
+// Nixe
+const nixe = new Nixe({
 	nixe: '.logo-img',
 	nixeImg1: 'assets/img/nixe/nixe-hands-up.png',
 	nixeImg2: 'assets/img/nixe/Nixe-cartel.png'
@@ -60,6 +65,9 @@ window.addEventListener('scroll', function(){
 
 	// Header
 	header.scroll();
+
+	// Nixe
+	nixe.scroll();
 })
 
 // ON RESIZE
@@ -69,9 +77,11 @@ window.addEventListener('resize', function(){
 });
 
 //DOM LOADED
-window.addEventListener('DOMContentLoaded', function(){
-	// Media Queries
-	const mql = e => {
+// window.addEventListener('DOMContentLoaded', function(){});
+
+// Media Queries
+	// Media Queries General
+	const sidenavMQ = e => {
 		var instances;
 		if (e.matches) {
 			// Sidenav
@@ -80,9 +90,6 @@ window.addEventListener('DOMContentLoaded', function(){
 			for(let i of $navItems){
 				i.classList.add('waves');
 			}
-
-			// Header
-			header.setNixe('.brand-img');
 		}else{
 			// Sidenav
 			$body.style.overflow = '';
@@ -90,14 +97,30 @@ window.addEventListener('DOMContentLoaded', function(){
 			for(let i of $navItems){
 				i.classList.remove('waves');
 			}
-
-			// Header
-			header.setNixe('.logo-img');
 		}
 	}
 
-	$desktopBpMd.addListener(mql);
-	mql($desktopBpMd);
-});
+	$desktopBpMd.addListener(sidenavMQ);
+	sidenavMQ($desktopBpMd);
+
+	// Media Queries Nixe
+	const nixeMQ = e => {
+		if (e.matches) {
+			nixe.changeValues({
+				nixe: '.nixe-responsive',
+				nixeImg1: 'assets/img/nixe/Nixe-up.png',
+				nixeImg2: 'assets/img/nixe/Nixe.png'
+			});
+		}else{
+			nixe.changeValues({
+				nixe: '.logo-img',
+				nixeImg1: 'assets/img/nixe/nixe-hands-up.png',
+				nixeImg2: 'assets/img/nixe/Nixe-cartel.png'
+			});
+		}
+	}
+
+	$desktopBpMd.addListener(nixeMQ);
+	nixeMQ($desktopBpMd);
 
 
