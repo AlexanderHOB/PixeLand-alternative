eba8a10992eb665a63d8527f46461c0fdab29433
Lercc <rokekanto@gmail.com>
2019-05-04 17:23:13 -0500

    footer responsiveterminado

--- Luis/assets/js/main.js
+++ Luis/assets/js/main.js
@@ -1,7 +1,14 @@
+//year
+const year=new Year({
+	id: 'year'
+})
+
 //DOM Loaded
 window.addEventListener('DOMContentLoaded', function() {
 	const loader = new Loader({
 		id: 'loader'
 	});
 	loader.start();
+
+	year.getYear();
 });
---
+++ Luis/assets/js/year.js
@@ -0,0 +1,10 @@
+class Year{
+    constructor(object){
+        this.id=document.getElementById(object.id);
+        this.date=new Date();
+    
+    }
+    getYear(){
+        this.id.innerHTML=this.date.getFullYear();
+    }
+}
